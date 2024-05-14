63fda4d59d75becb30b16a7daef5223a1c905647
Ghulam Akbar <ghulamakbarabbbro110@gmail.com>
2024-05-14 12:07:12 +0500

    updated

--- Day 19/main.js
+++ Day 19/main.js
@@ -13,35 +13,25 @@ var name_object = mix_Object.filter(function (value) { return typeof value === "
 console.log(name_object);
 var number_object = mix_Object.filter(function (value) { return typeof value === "number"; });
 console.log(number_object);
-var total_marks = 850;
-var one_percent_total_marks = 850 / 100;
-var grade = function (num) {
-    var percetage = num / one_percent_total_marks;
-    console.log(percetage);
-    if (percetage >= 90) {
-        console.log("Grade : A+ \nCongratulations on your fantastic grades!");
-    }
-    else if (percetage >= 80) {
-        console.log("Grade : A \nWell done! Your hard work paid off!");
-    }
-    else if (percetage >= 70) {
-        console.log("Grade : B \nGreat job on your outstanding grades!");
-    }
-    else if (percetage >= 60) {
-        console.log("Grade : C \nImpressive work! Keep it up!");
-    }
-    else if (percetage >= 40) {
-        console.log("Pass \nKeep focusing");
-    }
-    else if (percetage >= 0) {
-        console.log("Fail \nTyr agian");
-    }
-};
-grade(170);
-// A list of grades
-var grades = [88, 94, 72, 99, 53, 77];
-// Calculates the average grade
-var averageGrade = grades.reduce(function (total, grade) { return total + grade; }, 0) / grades.length;
-// Shows the average grade
-console.log(averageGrade);
-// First, we add up all the grades. Then, we divide by how many grades there are to get the average.
+var grades = [450, 500, 380, 570];
+var average = grades.reduce(function (num1, num2) { return num1 + num2; }, 0) / grades.length;
+console.log(average);
+// let total_marks:number =  850
+// let one_percent_total_marks:number =  850/100
+// let grade = (num:number):void=>{
+//     let percetage:number = num/one_percent_total_marks
+//     console.log(percetage)
+//     if (percetage >= 90 ){
+//         console.log("Grade : A+ \nCongratulations on your fantastic grades!")}
+//     else if (percetage >= 80){
+//         console.log("Grade : A \nWell done! Your hard work paid off!")}
+//     else if (percetage >= 70){
+//         console.log("Grade : B \nGreat job on your outstanding grades!")}
+//     else if (percetage >= 60){
+//         console.log("Grade : C \nImpressive work! Keep it up!")}
+//     else if (percetage >= 40){
+//         console.log("Pass \nKeep focusing")}
+//     else if (percetage >= 0){
+//         console.log("Fail \nTyr agian")}
+// }
+// grade(500)
--- Day 19/main.ts
+++ Day 19/main.ts
@@ -1,4 +1,5 @@
 // Q-55 Double Numbers In Array
+
 // forEach method
 let numberObj:number[] = [10,20,30,40,50]
 numberObj.forEach((value)=>{console.log(value*2)})
@@ -9,6 +10,7 @@ let DoubleNumbers = numberObj.map(num=>num*2)
 console.log(DoubleNumbers)
 // map method will work on copy array
 
+
 // Q-56 Mixed Value Array
 let mix_Object:(string|number)[] = ["Akbar","Ahmed","Raza","Bakir",123,456,789]
 let name_object = mix_Object.filter((value) => typeof value === "string")
@@ -16,33 +18,35 @@ console.log(name_object)
 let number_object = mix_Object.filter((value) => typeof value === "number")
 console.log(number_object)
 
-let total_marks:number =  850
-let one_percent_total_marks:number =  850/100
-let grade = (num:number):void=>{
-    let percetage:number = num/one_percent_total_marks
-    console.log(percetage)
-    if (percetage >= 90 ){
-        console.log("Grade : A+ \nCongratulations on your fantastic grades!")}
-    else if (percetage >= 80){
-        console.log("Grade : A \nWell done! Your hard work paid off!")}
-    else if (percetage >= 70){
-        console.log("Grade : B \nGreat job on your outstanding grades!")}
-    else if (percetage >= 60){
-        console.log("Grade : C \nImpressive work! Keep it up!")}
-    else if (percetage >= 40){
-        console.log("Pass \nKeep focusing")}
-    else if (percetage >= 0){
-        console.log("Fail \nTyr agian")}
-}
-grade(500)
+
+// Q-57 Finding Average
+let grades:number[] = [450,500,380,570];
+let average:number = grades.reduce((num1,num2)=>num1+num2,0)/grades.length
+console.log(average)
 
 
-// A list of grades
-let grades = [88, 94, 72, 99, 53, 77];
 
-// Calculates the average grade
-let averageGrade = grades.reduce((total, grade) => total + grade, 0) / grades.length;
 
-// Shows the average grade
-console.log(averageGrade);
-// First, we add up all the grades. Then, we divide by how many grades there are to get the average.
+
+
+
+
+// let total_marks:number =  850
+// let one_percent_total_marks:number =  850/100
+// let grade = (num:number):void=>{
+//     let percetage:number = num/one_percent_total_marks
+//     console.log(percetage)
+//     if (percetage >= 90 ){
+//         console.log("Grade : A+ \nCongratulations on your fantastic grades!")}
+//     else if (percetage >= 80){
+//         console.log("Grade : A \nWell done! Your hard work paid off!")}
+//     else if (percetage >= 70){
+//         console.log("Grade : B \nGreat job on your outstanding grades!")}
+//     else if (percetage >= 60){
+//         console.log("Grade : C \nImpressive work! Keep it up!")}
+//     else if (percetage >= 40){
+//         console.log("Pass \nKeep focusing")}
+//     else if (percetage >= 0){
+//         console.log("Fail \nTyr agian")}
+// }
+// grade(500)
