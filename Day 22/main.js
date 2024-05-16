1795807f4a4f598e13e6bcbded9d40f4b22d816e
Ghulam Akbar <ghulamakbarabbbro110@gmail.com>
2024-05-16 10:53:11 +0500

    updated

---
+++ Day 22/main.js
@@ -0,0 +1,16 @@
+// Q-64 Combine Strings and Numbers
+var function1 = function (text, num) {
+    console.log(text + num);
+};
+function1("age: ", 20);
+// Q-65 Determine the Remainder
+var function2 = function (num1, num2) {
+    console.log(num1 % num2);
+};
+function2(5, 2);
+// Q-66 Logical AND Verification
+var function3 = function (val1, val2) {
+    return val1 && val2;
+};
+var result = function3(true, false);
+console.log(result);
---
+++ Day 22/main.ts
@@ -0,0 +1,18 @@
+// Q-64 Combine Strings and Numbers
+let function1 = (text:string,num:number):void=>{
+    console.log(text+num)
+}
+function1("age: ",20)
+
+// Q-65 Determine the Remainder
+let function2 = (num1:number,num2:number):void=>{
+    console.log(num1 % num2)
+}
+function2(5,2)
+
+// Q-66 Logical AND Verification
+let function3 = (val1:boolean,val2:boolean):boolean=>{
+    return val1 && val2
+    }
+let result = function3(true,false)
+console.log(result)
