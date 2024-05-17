f831df16711bd7d767725731c3761b6e5d749694
Ghulam Akbar <ghulamakbarabbbro110@gmail.com>
2024-05-17 19:08:34 +0500

    Updated

---
+++ Day 29/main.js
@@ -0,0 +1,5 @@
+var function1 = function (name) {
+    var replaceing = name.replace(/city/g, "Town");
+    console.log(replaceing);
+};
+function1("I belog to Larkana city I love that city");
---
+++ Day 29/main.ts
@@ -0,0 +1,4 @@
+// Q-85 Finding the position of substring
+let substring_position = (val1:string)=>{
+    let result = val1.indexOf(/code/g,)
+}
